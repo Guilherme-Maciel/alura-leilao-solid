58f71e2aec1e9164988010c7414b13d87bf450ad
Guilherme Santos Maciel <guilhermemaciel@plusoft.com>
2023-01-13 22:24:32 -0300

    Add project files.

---
+++ src/Alura.LeilaoOnline.WebApp/Controllers/LeilaoApiController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Alura.LeilaoOnline.WebApp.Models;
+using Alura.LeilaoOnline.WebApp.Dados;
+
+namespace Alura.LeilaoOnline.WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/leiloes")]
+    public class LeilaoApiController : ControllerBase
+    {
+        AppDbContext _context;
+        LeilaoDaoComEfCore _dao;
+
+        public LeilaoApiController()
+        {
+            _context = new AppDbContext();
+            _dao = new LeilaoDaoComEfCore();
+        }
+
+        [HttpGet]
+        public IActionResult EndpointGetLeiloes()
+        {
+            var leiloes = _dao.BuscarLeiloes();
+            return Ok(leiloes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EndpointGetLeilaoById(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            return Ok(leilao);
+        }
+
+        [HttpPost]
+        public IActionResult EndpointPostLeilao(Leilao leilao)
+        {
+            _dao.Incluir(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpPut]
+        public IActionResult EndpointPutLeilao(Leilao leilao)
+        {
+            _dao.Alterar(leilao);
+            return Ok(leilao);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult EndpointDeleteLeilao(int id)
+        {
+            var leilao = _dao.BuscarPorId(id);
+            if (leilao == null)
+            {
+                return NotFound();
+            }
+            _dao.Excluir(leilao);
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDaoComEfCore.cs
@@ -0,0 +1,50 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDaoComEfCore
+    {
+        AppDbContext _context;
+
+        public LeilaoDaoComEfCore()
+        {
+            _context = new AppDbContext();
+                
+        }
+        public IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria)
+                .ToList();
+        }
+
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.First(l => l.Id == id);
+        }
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChanges();
+        }
+        public void Alterar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+    }
+}
